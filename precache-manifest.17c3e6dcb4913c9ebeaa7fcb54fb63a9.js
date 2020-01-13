79072087afdc63d1461b5846e6cfc515af836a50
Sergey <lobalmax@gmail.com>
2020-01-14 00:16:35 +0200

    Updates

---
+++ precache-manifest.17c3e6dcb4913c9ebeaa7fcb54fb63a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3767430f9740aa212f163f42ede091a4",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "0edf9ec58bee0a6cf80b",
+    "url": "/trello-clone/static/css/main.b95cac37.chunk.css"
+  },
+  {
+    "revision": "fc7bc2568f83fb751a72",
+    "url": "/trello-clone/static/js/2.92426ab2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trello-clone/static/js/2.92426ab2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0edf9ec58bee0a6cf80b",
+    "url": "/trello-clone/static/js/main.eb42ab40.chunk.js"
+  },
+  {
+    "revision": "525aa535c34a9f9cf6e5",
+    "url": "/trello-clone/static/js/runtime-main.468ee977.js"
+  },
+  {
+    "revision": "a5e64440cd3ce17ca542be43f24bdba7",
+    "url": "/trello-clone/static/media/bg-1.a5e64440.jpg"
+  },
+  {
+    "revision": "0592ecbecaf9e23b1f6904c916341512",
+    "url": "/trello-clone/static/media/user-superstar-2x.0592ecbe.jpg"
+  }
+]);
